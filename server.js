005ad67c25ac8ee6dade23fe1ac1ca873edfe2fb
MustafaGitZa <sibusisomst@gmail.com>
2025-06-15 03:25:24 +0200

    .

--- server.js
+++ server.js
@@ -60,6 +60,7 @@ function connectToDatabase() {
     db = mysql.createConnection({
       host: process.env.DB_HOST,
       user: process.env.DB_USER,
+       port: process.env.DB_PORT,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
     });
